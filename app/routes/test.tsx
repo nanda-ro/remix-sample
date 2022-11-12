8b9f3139870ded0e9377188d27a79765232aa611
yuuki <you@nanda.ro>
2022-11-12 14:10:27 +0000

    add: test page

---
+++ app/routes/test.tsx
@@ -0,0 +1,12 @@
+export default function Test() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          Test Page
+        </li>
+      </ul>
+    </div>
+  );
+}
